7d60b43389e178320e0c4623f836fa520a6dbe22
Joost Hartkamp <joosthartkamp@hotmail.com>
2019-02-11 11:15:06 +0100

    nu is arduino project

---
+++ bombmanV2.0/bombmanV2.0/bombmanV2.0.ino
@@ -0,0 +1,20 @@
+/*
+ * bombmanV2.ino
+ *
+ * Created: 2/11/2019 11:11:58 AM
+ * Author: joost
+ */ 
+
+void setup()
+{
+
+	  /* add setup code here, setup code runs once when the processor starts */
+
+}
+
+void loop()
+{
+
+	  /* add main program code here, this code starts again each time it ends */
+
+}
