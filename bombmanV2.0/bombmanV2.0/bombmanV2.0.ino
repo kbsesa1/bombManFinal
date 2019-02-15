331445e00d5df544d3f0703b57fb7d35358ee026
Joost <joosthartkamp@hotmail.com>
2019-02-15 15:52:30 +0100

    ir library created

--- bombmanV2.0/bombmanV2.0/bombmanV2.0.ino
+++ bombmanV2.0/bombmanV2.0/bombmanV2.0.ino
@@ -1,280 +1,157 @@
-//defining the sending times
-#define START 2000
-#define STOP 1600
-#define HIGH 1200
-#define LOW 800
-#define SPACE 400
 
-#define BUFFERLENGTH 36
+//Libraries--------
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
+#include <Adafruit_STMPE610.h>
+#include <ArduinoNunchuk.h>
+#include <gfxfont.h>
+#include <TaskScheduler.h>
+#include <SPI.h>
+//project specific
+#include "lib/irComm/irComm.h"
 
-//variables for decoding the IR protocol
-const uint16_t decodeTimes38[] = {45,75,105,135,180};
-	const uint16_t decodeTimes56[] = {60,120,160,200,250};
-		uint16_t minLength;
-		uint16_t maxLength;
-		bool recieveSpeed = false;
-		
-			
-volatile uint16_t decodeTicks = 0;
-volatile uint16_t times[BUFFERLENGTH];
-volatile uint16_t timeIndex = 0;
-unsigned long printTime = 0;
-unsigned long sendTime = 0;
+//Defines----------
+//TFT screen
+#define tft_cs  10
+#define tft_dc 9
+
+//Touchscreen
+#define stmpe_cs 8
+
+//Colors
+#define black 0x0000
+#define brown 0xB3CA
+#define gold  0xFE41
+#define grey  0x7BEF
+
+//Make objects----------
+Scheduler game;
+IR ir = IR(false);
+Adafruit_ILI9341 tft = Adafruit_ILI9341(tft_cs, tft_dc);
+Adafruit_STMPE610 ts = Adafruit_STMPE610(stmpe_cs);
+ArduinoNunchuk nunchuck = ArduinoNunchuk();
+
+//Variables----------
+//test
+uint8_t knippertest = 0;
+uint8_t xcord = 0;
+uint32_t data = 1024;
 
 
-uint8_t outPacket[34];
-uint8_t inPacket[34];
-uint32_t sendBuffer = 0;
+
+//Functions----------
+//Test
+void driehoek();
+void vierkant();
+
+//draw buttons
+void drawButton(uint8_t xl, uint8_t yl, uint8_t xc, uint8_t yc);
+void drawLobby();
+void drawText(uint8_t xl,uint8_t yl, uint8_t textsize, char * string);
+
+//move
+void moveLeft();
+void moveRight();
+void moveUp();
+void moveDown();
 
 
 
+//Tasks----------
+Task tekenDriehoek(2000, -1, &driehoek);
+Task tekenVierkant(333, -1, &vierkant);
+Task irCommunication(100, -1, &irSend);
+Task printIRData(1000, -1, &irPrint);
 
-
-
-
-int main(void){
-	
+int main(){
+	//initialisation process Arduino UNO
 	init();
-	DDRD |= (1<<PORTD4);
-	Serial.begin(115200);
-	initIRSend(false);
-	initIRRecieve();
-	//setIR(true);
-	sendBuffer = 255;
+	
+	//initialisation process task scheduler
+	game.init();
+	game.addTask(irCommunication);
+	game.addTask(printIRData);
+	irCommunication.enable();
+	printIRData.enable();
+	//game.addTask(tekenDriehoek);
+	//game.addTask(tekenVierkant);
+	//tekenVierkant.enable();
+	
+	//initialisation process infrared communication
+	ir.begin();
+	
+	/*
+	//initialisation process tft screen
+	tft.begin();
+	tft.setRotation(1);
+
+	//initialisation process touchscreen support
+	ts.begin();
+
+	//initialisation process Wii nunchuck
+	DDRC |= (1<<PORTC2) | (1<<PORTC3);
+	PORTC |= (1<<PORTC3);
+	nunchuck.init();
+*/
+
 	while(1){
-		
-		if (millis() >= sendTime + 50)
-		{
-			sendTime = millis();
-			buildPacket(outPacket,sendBuffer);
-			//printPacket(outPacket);
-			sendPacket(outPacket);
-			sendBuffer++;
-		}
-		
-		if (millis() >= printTime + 50)
-		{
-			printTime = millis();
-			
-						
-			//printTimes();
-			decodeIR(inPacket);
-			//printPacket(inPacket);
-			Serial.print("value: ");
-			Serial.println(parseIR(inPacket));
-			
-		}
-		
-		
-	}
-	return 0;
-}
-ISR(TIMER2_COMPB_vect){
-	TCNT2 = 0;
-	decodeTicks++;
-	if (decodeTicks >= 500)
-	{
-		decodeTicks = 500;
-	}
-}
-ISR(INT0_vect) //interrupt ISR
-{
-	uint16_t pulseLength = decodeTicks;
-	decodeTicks = 0;
-	if (pulseLength >= minLength)
-	{
-		times[timeIndex] = pulseLength;
-		timeIndex++;
-	}
-	if (pulseLength >= maxLength){
-		timeIndex = 0;
+		game.execute();
 	}
 }
 
-void initIRSend(bool highspeed){
-	recieveSpeed = highspeed;
-	if (recieveSpeed)
-	{
-		minLength = decodeTimes56[0];
-		maxLength = decodeTimes56[4];
-		}else{
-		minLength = decodeTimes38[0];
-		maxLength = decodeTimes38[4];
+void driehoek(){
+	tft.fillRect(xcord,20,50,50, 0xFE41 );
+	xcord += 20;
+	if(xcord == 200){
+		xcord = 0;
 	}
-	//set ir led port to output
-	DDRD |= (1 << PORTD3);
-	
-	//set waveform generation to normal mode
-	TCCR2A &= ~(1 << WGM20);
-	TCCR2A &= ~(1 << WGM21);
-	TCCR2B &= ~(1 << WGM22);
-	
-	
-	//set prescaler to 1
-	TCCR2B |= (1 << CS20);
-	TCCR2B &= ~(1 << CS21);
-	TCCR2B &= ~(1 << CS22);
-	//set output compare register to 209 for 38KHz
-	if (highspeed)
-	{
-		OCR2B = 118;
-		
-	}
-	else
-	{
-		OCR2B = 182;
-	}
-	//enable output compare interrupt
-	TIMSK2 |= (1 << OCIE2B);
-	sei();
-	setIR(false);
-	
-}
-void initIRRecieve(){
-	
-	DDRD &= ~(1<<PORTD2);
-	PORTD &= ~(1<<PORTD2);
-	EIMSK |= (1 << INT0);    /* enable INT0 */
-	EICRA |= (1<<ISC00);    /*falling edge interrupt*/
-	sei();  //enable interrupts
-}
-void setIR(bool state){
-	if (state){
-		//set timer output to toggle on 38Khz
-		TCCR2A |= (1<<COM2B0);
-		TCCR2A &= ~(1<<COM2B1);
-	}
-	else{
-		//disconnect timer output
-		TCCR2A &= ~(1<<COM2B0);
-		TCCR2A &= ~(1<<COM2B1);
-	}	
-}
-void sendPulse(uint16_t length){
-	unsigned long lastPulse = micros();
-	setIR(true);
-	while(micros() < lastPulse+length){
-		
-	}
-	lastPulse = micros();
-	setIR(false);
-	while(micros() < lastPulse+SPACE){
-		
-	}
-	lastPulse = micros();
-}
-void buildPacket(uint8_t *packetBuffer ,uint32_t data){
-	uint8_t packetIndex = 0;
-	packetBuffer[packetIndex] = 4;
-	packetIndex ++;
-	for(int i = 0;i<32;i++){
-		uint32_t masked = 0;
-		masked = (data>>i) & 1;
-		if (masked)	packetBuffer[packetIndex] = 2;
-		else packetBuffer[packetIndex] = 1;
-		packetIndex++;
-	}
-	packetBuffer[packetIndex] = 3;
-}
-void printPacket(uint8_t *packetBuffer){
-	Serial.println("packet contents:");
-	Serial.print("(");
-	for (int i = 0;i<34;i++){
-		switch(packetBuffer[i]){
-			case 4:
-			Serial.print("ST,");
-			break;
-			case 2:
-			Serial.print("H,");
-			break;
-			case 1:
-			Serial.print("L,");
-			break;
-			case 3:
-			Serial.print("STP");
-			break;
-		}
-	}
-	Serial.println(")");
-}
-void sendPacket(uint8_t *packetBuffer){
-	for (int i = 0;i<34;i++)
-	{
-		switch(packetBuffer[i]){
-			case 1:
-			sendPulse(LOW);
-			break;
-			case 2:
-			sendPulse(HIGH);
-			break;
-			case 3:
-			sendPulse(STOP);
-			break;
-			case 4:
-			sendPulse(START);
-			break;
-		}
-	}
-}
-void sendTestMessage(){
-	for (int i = 0;i<sizeof(outPacket)/sizeof(int);i++)
-	{
-		sendPulse(outPacket[i]);
-	}
-}
-void decodeIR(uint8_t *packetBuffer){
-	uint8_t packetIndex = 0;
-	for (int i = 0;i<BUFFERLENGTH;i++)
-	{
-		uint16_t pulseType = 0;
-		for (int j = 4;j>0;j--)
-		{
-			if (recieveSpeed)
-			{
-				if (times[i] > decodeTimes56[j-1]&&times[i]< decodeTimes56[j])
-				{
-					pulseType = j;
-				}
-			}
-			else{
-			if (times[i] > decodeTimes38[j-1]&&times[i]< decodeTimes38[j])
-			{
-				pulseType = j;
-			}	
-			}
-			
-		}
-		if (pulseType != 0)
-		{
-			packetBuffer[packetIndex] = pulseType;
-			packetIndex++;
-			if (packetIndex >= 34)
-			{
-				return;
-			}
-		}
-			
-			
-		
-	}
-}
-uint32_t parseIR(uint8_t *packetBuffer){
-	uint32_t output=0;
-	for (int i = 1;i<32;i++)
-	{
-		output |= (packetBuffer[i]-1)<<(i-1);
-	}
-	return output;
-}
-void printTimes(){
-	Serial.print("times: (");
-	for (int i = 0;i<BUFFERLENGTH;i++)
-	{
-		Serial.print(times[i]);
-		Serial.print(",");
-	}
-	Serial.println(")");
 }
 
+void vierkant(){
+	if(knippertest == 0){
+		tft.fillRect(180,180, 50,50,0xF921);
+		knippertest = 1;
+		} else {
+		tft.fillRect(180,180, 50,50,0xF81F);
+		knippertest = 0;
+	}
+}
 
+// Draws two sqaures to make a button
+void drawButton(uint8_t xc, uint8_t yc, uint8_t xl, uint8_t yl){
+	#define offset 5    //thickness of the brown border
+	tft.fillRect(xc,yc,xl,yl,brown);
+	tft.fillRect(xc+offset,yc+offset, xl-(2*offset), yl-(2*offset), gold);
+}
 
+/*
+// Draws the lobbyscreen
+void drawLobby(){
+	tft.fillScreen(grey);
+	tft.setTextColor(gold);
+	tft.setCur
+	drawButton(20, 60, 90, 110);
+	tft.setTextColor(black);
+	tft.setTextSize(5);
+	tft.setCursor(28,98);
+	tft.print("GO!");
+}
+
+// Draws text
+void drawText(uint8_t xl, uint8_t yl, uint8_t textsize, ..){
+	tft.setCursor(xl, yl);
+	tft.setTextSize(textsize);
+	tft.println(text);
+}
+*/
+
+void irSend(){
+	ir.run();
+	ir.send(data);
+	data ++;
+}
+void irPrint(){
+	Serial.print("i got: ");
+	uint32_t value = ir.read();
+	Serial.println(value);
+}
---
+++ bombmanV2.0/bombmanV2.0/lib/irComm/irComm.h
@@ -0,0 +1,32 @@
+// irComm.h
+
+#ifndef _IRCOMM_h
+#define _IRCOMM_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+class IR
+{
+public:
+IR(bool highFrequency = false);
+void begin();
+void run();
+void send(uint32_t data);
+uint32_t read();
+protected:
+private:
+void initSend();
+void initRecieve();
+void setIR(bool state);
+void sendPulse(uint16_t length);
+void buildPacket(uint8_t *packetBuffer ,uint32_t data);
+void sendPacket(uint8_t *packetBuffer);
+void decode(uint8_t *packetBuffer);
+uint32_t parse(uint8_t *packetBuffer);
+};
+
+#endif
+
